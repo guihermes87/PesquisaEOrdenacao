a102b423c813b780a4f91d7a3a058319d1629beb
Guilherme Hermes <129190225+guihermes87@users.noreply.github.com>
2026-08-17 08:17:33 -0300

    Add files via upload

---
+++ Projetos/Ordenacao/View/Visualizacao.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+
+public class Visualizacao
+{
+    public static void exibir(List<int> lista)
+    {
+        foreach (var item in lista)
+        {
+            Console.WriteLine(item.ToString());
+        }
+    }
+
+    public static void exibirTempo(Stopwatch sw, string frase)
+    {
+        Console.WriteLine(frase + " (ms): " + sw.ElapsedMilliseconds);
+    }
+
+    public static void exibirTrocas(string nome, int qtdTrocas)
+    {
+        Console.WriteLine(nome + " - trocas: " + qtdTrocas);
+    }
+}
